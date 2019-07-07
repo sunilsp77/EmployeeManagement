0ace80dca66fd8f7a96035565a5dfb4a4095e2f4
Sunil Prakash <sunil.thind17@gmail.com>
2019-07-07 21:40:51 +0530

    Form can be saved to db

--- src/containers/EmployeeBuilder/EmployeeBuilder.js
+++ src/containers/EmployeeBuilder/EmployeeBuilder.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 import Input from '../../components/UI/Input/Input';
+import classes from './EmployeeBuilder.module.css';
+import Button from '../../components/UI/Button/Button';
+import axios from '../../axios-empdata';
 
 class EmployeeBuilder extends Component {
   state = {
@@ -12,6 +15,7 @@ class EmployeeBuilder extends Component {
           placeholder: 'Your Name',
         },
         value: '',
+        label: 'Name',
       },
       gender: {
         elementType: 'input',
@@ -19,6 +23,7 @@ class EmployeeBuilder extends Component {
           type: 'radio',
         },
         value: 'male',
+        label: 'Gender',
       },
       birthdate: {
         elementType: 'input',
@@ -28,6 +33,7 @@ class EmployeeBuilder extends Component {
           max: '2050-01-01',
         },
         value: '2018-07-22',
+        label: 'Birthdate',
       },
       photo: {
         elementType: 'input',
@@ -35,6 +41,7 @@ class EmployeeBuilder extends Component {
           type: 'file',
           accept: 'image/png, image/jpeg',
         },
+        label: 'Image',
       },
     },
     home_workInfo: {
@@ -46,6 +53,7 @@ class EmployeeBuilder extends Component {
           placeholder: 'Mailing Address',
         },
         value: '',
+        label: 'Mailing Address',
       },
       phone: {
         elementType: 'input',
@@ -54,6 +62,7 @@ class EmployeeBuilder extends Component {
           placeholder: 'Phone number',
         },
         value: '',
+        label: 'Contact number',
       },
       email: {
         elementType: 'input',
@@ -62,16 +71,44 @@ class EmployeeBuilder extends Component {
           placeholder: 'Email',
         },
         value: '',
+        label: 'Email',
       },
+      linkedIn: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'url',
+          placeholder: 'https://example.com',
+          pattern: 'https://.*',
+          size: '30',
+        },
+        value: '',
+        label: 'LinkedIn Url',
+      },
+    },
+    jobDetails: {
+      department: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            { value: 'diagnostics', displayValue: 'Diagnostics' },
+            { value: 'hmi', displayValue: 'HMI' },
+            { value: 'application', displayValue: 'Application' },
+          ],
+        },
+        value: 'diagnostics',
+        label: 'Department',
+      },
+      //   skills: {
+      //       elementType: 'input',
+      //       elementConfig:
+      //   }
     },
   };
 
   personalInfoChangedHandler = (event, inputIdentifier) => {
     const updatedForm = { ...this.state.personalInfo };
     const updatedFormElement = { ...updatedForm[inputIdentifier] };
-
     updatedFormElement.value = event.target.value;
-
     updatedForm[inputIdentifier] = updatedFormElement;
     this.setState({
       personalInfo: updatedForm,
@@ -80,17 +117,40 @@ class EmployeeBuilder extends Component {
   home_workInfoChangedHandler = (event, inputIdentifier) => {
     const updatedForm = { ...this.state.home_workInfo };
     const updatedFormElement = { ...updatedForm[inputIdentifier] };
-
     updatedFormElement.value = event.target.value;
-
     updatedForm[inputIdentifier] = updatedFormElement;
     this.setState({
       home_workInfo: updatedForm,
     });
   };
+  jobDetailsChangedHandler = (event, inputIdentifier) => {
+    const updatedForm = { ...this.state.jobDetails };
+    const updatedFormElement = { ...updatedForm[inputIdentifier] };
+    updatedFormElement.value = event.target.value;
+    updatedForm[inputIdentifier] = updatedFormElement;
+    this.setState({
+      jobDetails: updatedForm,
+    });
+  };
+  formDataHandler = event => {
+    event.preventDefault();
+    const formData = {};
+    for (let formElement in this.state.personalInfo) {
+      formData[formElement] = this.state.personalInfo[formElement].value;
+    }
+    const empData = {
+      personalInfo: formData,
+    };
+
+    axios
+      .post('./employeeData.json', empData)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
+  };
   render() {
     let personalInfoArray = [],
-      home_workInfoArray = [];
+      home_workInfoArray = [],
+      jobDetailsInfoArray = [];
     for (let key in this.state.personalInfo) {
       personalInfoArray.push({
         id: key,
@@ -103,9 +163,15 @@ class EmployeeBuilder extends Component {
         config: this.state.home_workInfo[key],
       });
     }
+    for (let key in this.state.jobDetails) {
+      jobDetailsInfoArray.push({
+        id: key,
+        config: this.state.jobDetails[key],
+      });
+    }
 
     let form = (
-      <form>
+      <form onSubmit={this.formDataHandler}>
         <fieldset>
           <legend>Personal information</legend>
           {personalInfoArray.map(formElement => (
@@ -114,6 +180,7 @@ class EmployeeBuilder extends Component {
               elementType={formElement.config.elementType}
               elementConfig={formElement.config.elementConfig}
               value={formElement.config.value}
+              label={formElement.config.label}
               changed={event =>
                 this.personalInfoChangedHandler(event, formElement.id)
               }
@@ -128,17 +195,34 @@ class EmployeeBuilder extends Component {
               elementType={formElement.config.elementType}
               elementConfig={formElement.config.elementConfig}
               value={formElement.config.value}
+              label={formElement.config.label}
               changed={event =>
                 this.home_workInfoChangedHandler(event, formElement.id)
               }
             />
           ))}
         </fieldset>
+        <fieldset>
+          <legend>Job Details</legend>
+          {jobDetailsInfoArray.map(formElement => (
+            <Input
+              key={formElement.id}
+              elementType={formElement.config.elementType}
+              elementConfig={formElement.config.elementConfig}
+              value={formElement.config.value}
+              label={formElement.config.label}
+              changed={event =>
+                this.jobDetailsChangedHandler(event, formElement.id)
+              }
+            />
+          ))}
+        </fieldset>
+        <Button btnType="Success">SUBMIT</Button>
       </form>
     );
     return (
-      <div>
-        <h1>Enter your Employee Data</h1>
+      <div className={classes.Content}>
+        <h1 className={classes.heading}>Enter your Employee Data</h1>
         {form}
       </div>
     );
